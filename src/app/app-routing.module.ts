ac60caf9efe201f4181376f208c10ea78b4feb01
rchlkm <rhkim@andrew.cmu.edu>
2017-12-22 01:42:11 -0800

    created span anchor... but toggle navbar fails

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,11 +6,11 @@ import { ProjectDetailComponent } from './project-detail/project-detail.componen
 import { AboutComponent }    from './about/about.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/projects', pathMatch: 'full' },
-  { path: 'projects', component: ProjectsComponent },
-  // { path: 'detail/:title', component: ProjectDetailComponent },
-  { path: 'detail/:id', component: ProjectDetailComponent },
-  { path: 'about', component: AboutComponent },
+  // { path: '', redirectTo: '/projects', pathMatch: 'full' },
+  // { path: 'projects', component: ProjectsComponent },
+  // // { path: 'detail/:title', component: ProjectDetailComponent },
+  // { path: 'detail/:id', component: ProjectDetailComponent },
+  // { path: 'about', component: AboutComponent },
 
 
 ];
