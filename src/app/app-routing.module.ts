a0eae0d8b72b061f1657d0c57eddc85ba38211ab
rchlkm <rhkim@andrew.cmu.edu>
2018-05-24 21:31:19 +0800

    reformatted components for better navigations

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { ProjectsComponent }    from './projects/projects.component';
 import { ProjectDetailComponent } from './project-detail/project-detail.component';
-import { AboutComponent }    from './about/about.component';
+import { HomeComponent }    from './home/home.component';
 
 const routes: Routes = [
-  // { path: '', redirectTo: '/projects', pathMatch: 'full' },
-  // { path: 'projects', component: ProjectsComponent },
-  // // { path: 'detail/:title', component: ProjectDetailComponent },
-  // { path: 'detail/:id', component: ProjectDetailComponent },
-  // { path: 'about', component: AboutComponent },
+	{ path: '', redirectTo: '/home', pathMatch: 'full' },
+	{ path: 'home', component: HomeComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'detail/:title', component: ProjectDetailComponent },
+  { path: 'detail/:id', component: ProjectDetailComponent },
 
 
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,19 @@ import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './/app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { ProjectsComponent } from './projects/projects.component';
-import { AboutComponent } from './about/about.component';
 import { ProjectDetailComponent } from './project-detail/project-detail.component';
 import { ProjectService } from './project.service';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ProjectsComponent,
-    AboutComponent,
-    ProjectDetailComponent
+    ProjectDetailComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
